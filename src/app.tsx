2fb4feebe9e21199958383e6c10a107b2bcd5efd
jiangyang <1140938202@qq.com>
2022-10-11 18:22:48 +0800

    fix:提交测试

--- src/app.tsx
+++ src/app.tsx
@@ -11,7 +11,7 @@ import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
 import '@/assets/style/app.less';
 const isDev = process.env.NODE_ENV === 'development';
 const loginPath = '/user/login';
-
+ 
 /**
  * @see  https://umijs.org/zh-CN/plugins/plugin-initial-state
  * */
