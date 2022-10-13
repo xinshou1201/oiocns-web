54bd1bff8621d3c97145906a9df65b55ba7fe774
SEN <changsen25@163.com>
2022-10-13 15:47:01 +0800

    fix: eslint事件绑定问题处理

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,8 @@ module.exports = {
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
     'react/react-in-jsx-scope': 'off',
     'jsx-a11y/accessible-emoji': 'off',
+    'jsx-a11y/no-static-element-interactions': [0],
+    'jsx-a11y/click-events-have-key-events': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,9 +19,16 @@ const Home: React.FC = () => {
   useEffect(() => {}, []);
   const store = useStore();
   console.log('store', store);
+  const fun = () => {
+    console.log('搜索');
+  };
 
   return (
-    <div className="work-home">
+    <div
+      className="work-home"
+      onClick={() => {
+        fun();
+      }}>
       <BannerCom imgList={imgList} />
     </div>
   );
