12db4fae8efcc0165abc5b0afb8d6dc68a377061
SEN <changsen25@163.com>
2022-10-13 16:06:27 +0800

    style: 样式文件修改

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,7 +3,7 @@ import './index.less';
 // import { Card } from 'antd';
 import React, { useEffect } from 'react';
 
-import BannerImg from '@/assets/imgs/banner1.png';
+import BannerImg from '@/assets/img/banner1.png';
 import useStore from '@/store';
 
 import BannerCom from './CustomCom/BannerCom';
