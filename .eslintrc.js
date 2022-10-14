fd9de12754fdfa5331b3c9faf29fc0492138fc6f
SEN <changsen25@163.com>
2022-10-14 15:14:02 +0800

    fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,12 +29,27 @@ module.exports = {
     'prettier/prettier': ['error', {}, { usePrettierrc: true }],
     'react/react-in-jsx-scope': 'off',
     'jsx-a11y/accessible-emoji': 'off',
-    'jsx-a11y/no-static-element-interactions': [0],
+    'jsx-a11y/no-static-element-interactions': ['off'],
     'jsx-a11y/click-events-have-key-events': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    'jsx-a11y/no-static-element-interactions': [0],
-    'jsx-a11y/click-events-have-key-events': 'off',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+    overrides: [
+      {
+        files: ['**/*.d.ts','**/*.tsx'],
+        rules: {
+          //解决ts中，本身是可以有空的构造函数的误判问题 START
+          'no-useless-constructor': 'off',
+          '@typescript-eslint/no-useless-constructor': 'error',
+          'no-unused-vars': 'error',
+          '@typescript-eslint/no-unused-vars': 'error',
+          'no-empty-function': 'off',
+          '@typescript-eslint/no-empty-function': 'error',
+          //解决导出类型时，no-undef报错问题
+          'no-undef': 'off',
+          'import/prefer-default-export': 'off',
+        },
+      },
+    ],
   },
 };
---
+++ typings/global.d.ts
@@ -0,0 +1,3 @@
+interface TestType {
+  name: string;
+}
