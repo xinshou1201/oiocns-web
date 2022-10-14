96184f40076ac368cf64801b0b346fe8f3cb41ca
cclu <lkrnchocolateaaron@gmail.com>
2022-10-14 10:14:41 +0800

    add: 图标地址修改

--- src/components/IconFont/index.tsx
+++ src/components/IconFont/index.tsx
@@ -2,6 +2,6 @@ import { createFromIconfontCN } from '@ant-design/icons';
 
 export const IconFont = createFromIconfontCN({
   scriptUrl: [
-    '//at.alicdn.com/t/c/font_3701577_qpd84m8zwz.js', // icon-javascript, icon-java, icon-shoppingcart (overrided)
+    '//at.alicdn.com/t/c/font_3701577_wk4juvgh2ps.js', // icon-javascript, icon-java, icon-shoppingcart (overrided)
   ],
 });
