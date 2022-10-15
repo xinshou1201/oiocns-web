6c4130427510821b6ed0ac89313d2247466154ec
SEN <changsen25@163.com>
2022-10-15 18:20:39 +0800

    other: 删除

--- src/pages/Market/index.tsx
+++ src/pages/Market/index.tsx
@@ -3,6 +3,7 @@ import './index.less';
 import { Layout } from 'antd';
 import React from 'react';
 import { renderRoutes } from 'react-router-config';
+
 import MarketClassify from './Classify';
 import MarketHeader from './Header';
 
@@ -11,7 +12,6 @@ const { Header, Sider, Content } = Layout;
 const Market: React.FC<any> = (props) => {
   const { route, history } = props;
 
-  console.log('搜索一样一样', route);
   return (
     <Layout className="market-wrap">
       <Sider width={250}>
