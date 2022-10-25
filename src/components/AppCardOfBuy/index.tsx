11b907a1503fec90874352a1fc4790e2151486f3
SEN <changsen25@163.com>
2022-10-25 16:41:23 +0800

    fix: 业务组件

--- src/components/AppCardOfBuy/index.tsx
+++ src/components/AppCardOfBuy/index.tsx
@@ -1,46 +1,50 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { CheckCard } from '@ant-design/pro-components';
 import { Button, Dropdown, Menu } from 'antd';
 import { EllipsisOutlined } from '@ant-design/icons';
 import cls from './index.module.less';
+import { MarketTypes } from 'typings/marketType';
 
-interface indexType {
+interface BuyAppType {
   className?: string;
   showBtn?: boolean; //是否展示按钮
+  data: any; //数据源
   shouOperation?: boolean; //是否展示 右上角操作按钮
-  // eslint-disable-next-line no-unused-vars
+  handleBuyApp: (_type: 'buy' | 'join', item: BuyAppType['data']) => void;
   onClick?: (e?: Event) => void; //卡片点击事件
+  operation?: (_item: MarketTypes.ProductType) => MarketTypes.OperationType[]; //操作区域数据
 }
 
-const menu = (
-  <Menu
-    items={[
-      {
-        key: '1',
-        label: '按钮1',
-      },
-      {
-        key: '2',
-        label: '按钮2',
-      },
-      {
-        key: '3',
-        label: '按钮3',
-      },
-    ]}
-  />
-);
-const Index: React.FC<indexType> = (props) => {
-  const { className, showBtn = true, shouOperation = false, onClick } = props;
-  console.log('打印index', props, shouOperation);
-
+const Index: React.FC<BuyAppType> = (props) => {
+  const {
+    data,
+    className,
+    showBtn = true,
+    shouOperation = false,
+    onClick,
+    operation,
+    handleBuyApp,
+  } = props;
+  /**
+   * @desc: 操作按钮区域
+   * @param {any} item - 表格单条数据 data
+   * @return {Menu} - 渲染 按钮组
+   */
+  const menu = () => {
+    return <Menu items={operation && operation(data)} />;
+  };
   function renderName() {
     return (
       <>
-        <h3 className={cls.nameLabel}>应用名称</h3>
-        <Dropdown overlay={menu} placement="bottom">
-          <EllipsisOutlined className={cls.operationBtn} />
-        </Dropdown>
+        <h3 className={cls.nameLabel}>{data['name']}</h3>
+        {shouOperation ? (
+          <Dropdown overlay={menu} placement="bottom">
+            <EllipsisOutlined className={cls.operationBtn} />
+          </Dropdown>
+        ) : (
+          ''
+        )}
       </>
     );
   }
@@ -50,10 +54,16 @@ const Index: React.FC<indexType> = (props) => {
         <p>选择一个由流程编排提供的典型用户案例，可以从。</p>
         {showBtn ? (
           <p className={cls.btnBox}>
-            <Button className={cls.btn} shape="round">
+            <Button
+              className={cls.btn}
+              shape="round"
+              onClick={() => handleBuyApp('join', data)}>
               加入购物车
             </Button>
-            <Button className={cls.btn} shape="round">
+            <Button
+              className={cls.btn}
+              shape="round"
+              onClick={() => handleBuyApp('buy', data)}>
               获取
             </Button>
           </p>
