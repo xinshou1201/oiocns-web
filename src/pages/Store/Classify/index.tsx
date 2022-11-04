a3f52ae121c4a6b8d3844cdb00f8ce7c424f1e1c
SEN <changsen25@163.com>
2022-11-04 14:42:37 +0800

    add:树形操作

--- src/pages/Store/Classify/index.tsx
+++ src/pages/Store/Classify/index.tsx
@@ -10,34 +10,39 @@ import { useHistory } from 'react-router-dom';
 
 import cls from './index.module.less';
 import StoreClassifyTree from '@/components/CustomTreeComp';
+const items = [
+  { label: '应用', key: 'app', icon: <AppstoreOutlined /> }, // 菜单项务必填写 key
+  { label: '文档', key: 'doc', icon: <FileTextOutlined /> },
+  { label: '数据', key: 'data', icon: <FundOutlined /> },
+  { label: '资源', key: 'src', icon: <DatabaseOutlined /> },
+];
 
+const menu = ['重命名', '创建副本', '拷贝链接', '移动到', '收藏', '删除'];
 const StoreClassify: React.FC = () => {
   const history = useHistory();
 
-  const items = [
-    { label: '应用', key: 'app', icon: <AppstoreOutlined /> }, // 菜单项务必填写 key
-    { label: '文档', key: 'doc', icon: <FileTextOutlined /> },
-    { label: '数据', key: 'data', icon: <FundOutlined /> },
-    { label: '资源', key: 'src', icon: <DatabaseOutlined /> },
-  ];
-
   //菜单跳转
-  const to = (e: any) => {
+  const goPage = (e: any) => {
     history.push(`/store/${e.key}`);
+  };
+  const handleAddShop = (item: any) => {
+    console.log('handleAddShop', item);
+  };
+  const handleMenuClick = ({ data, key }: { data: any; key: string }) => {
+    console.log('handleMenuClick', data, key);
   };
 
   return (
     <div className={cls.container}>
-      {/* <div className={cls.top}>
-        <div className={cls.title}>
-          <HomeOutlined />
-          <strong>仓库</strong>
-        </div>
-      </div> */}
       <div>
         <div className={cls.subTitle}>常用分类</div>
-        <Menu items={items} onClick={to} />
-        <StoreClassifyTree treeType="试试"></StoreClassifyTree>
+        <Menu items={items} onClick={goPage} />
+        <StoreClassifyTree
+          menu={menu}
+          searchable
+          handleAddClick={handleAddShop}
+          handleMenuClick={handleMenuClick}
+        />
       </div>
     </div>
   );
